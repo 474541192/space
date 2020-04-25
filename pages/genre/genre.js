6bf077c963cc9d4b098079133487db58f8f0f5c3
djy <Your_email>
2020-04-25 14:14:35 +0800

    djy-demo

---
+++ pages/genre/genre.js
@@ -0,0 +1,79 @@
+// pages/genre/genre.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+list:[
+{'id':1,'title':'全部商品',},
+{'id':2,'title':'防疫专区',},
+{'id':3,'title':'美妆护肤',},
+{'id':4,'title':'个护健康',},
+{'id':5,'title':'家居用品',},
+{'id':6,'title':'母婴用品',},
+{'id':7,'title':'生鲜美食',},
+{'id':8,'title':'酒水咖啡',},
+{'id':9,'title':'企业采购',},
+{'id':10,'title':'超值套餐',},
+{'id':11,'title':'品牌专区',},
+{'id':12,'title':'尾货清仓',}
+]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
